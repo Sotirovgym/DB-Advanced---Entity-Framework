f4080bc60245d2201fca252e96cbd39c86112da8
Georgi <sotirovgym@abv.bg>
2017-11-15 19:29:21 +0200

    Upload exercises

---
+++ EntityRelationsExercises/P01. StudentSystem/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var context = new StudentSystemContext();
+
+            ResetDatabase(context);
+        }
+
+        private static void ResetDatabase(StudentSystemContext context)
+        {
+            context.Database.EnsureDeleted();
+
+            context.Database.Migrate();
+        }
+    }
+}
---
+++ EntityRelationsExercises/P03_ FootballBetting.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_FootballBetting.Data
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=SOTIROVGYM-PC\SQLEXPRESS;Database=FootballBetting;Integrated Security=True;";
+    }
+}
